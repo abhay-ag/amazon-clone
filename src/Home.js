35d171cbb8a67548ca20ae0acfe8dc0ef7782daf
abhay_ag <abhay0034.be21@chitkara.edu.in>
2022-04-04 23:14:37 +0530

    Products Added | Home Page Complete

--- src/Home.js
+++ src/Home.js
@@ -13,15 +13,52 @@ function Home() {
         />
 
         <div className="home__row">
-            <Product />
-            <Product />
+            <Product 
+                id="111"
+                title='The lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback'
+                price={11.96}
+                image = "https://m.media-amazon.com/images/I/81RCff1NpnL._AC_UY218_.jpg"
+                rating={5} />
+            <Product 
+                id="112"
+                title='Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl'
+                price={239.0}
+                image = "https://m.media-amazon.com/images/I/61jhA1++G6L._AC_UY218_.jpg"
+                rating={4} />
         </div>
 
         <div className="home__row">
-          {/* Product */}
-          {/* Product */}
-          {/* Product */}
+            <Product 
+                    id="113"
+                    title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
+                    price={199.99}
+                    image = "https://m.media-amazon.com/images/I/81ncZ64JDCL._AC_UY218_.jpg"
+                    rating={3} />
+            <Product 
+                    id="114"
+                    title='Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric'
+                    price={98.99}
+                    image = "https://m.media-amazon.com/images/I/61KIy6gX-CL._AC_UY218_.jpg"
+                    rating={5} />
+
+            <Product 
+                    id="115"
+                    title='New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Space Grey (4th Generation)'
+                    price={589.99}
+                    image = "https://m.media-amazon.com/images/I/81+N4PFF7jS._AC_UY218_.jpg"
+                    rating={4} />
         </div>
+
+        <div className="home__row">
+            <Product 
+                id= "116"
+                title= "Samsung LC49HG90DMUXEN 48.9-inch Ultra Wide Curved Monitor (Black)"
+                price = {232.99}
+                rating = {4}
+                image  = "https://m.media-amazon.com/images/I/71MlcO29QOL._AC_UY218_.jpg"
+            />
+        </div>
+
         <div className="home__row">{/* Product */}</div>
       </div>
     </div>
--- src/Product.js
+++ src/Product.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import  './Product.css';
 
-function Product() {
+function Product({id,title, price, image, rating}) {
   return (
     <div className="product">
         <div className="product__info">
-            <p>The Lean Startup</p>
+            <p>{title}</p>
             <p className="product__price">
                 <small>$</small>
-                <strong>19.99</strong>
+                <strong>{price}</strong>
             </p>
             <div className="product__rating">
-                <p>⭐</p>
+                {Array(rating).fill().map((_, i) =>(
+                    <p>⭐</p>
+                ))}
             </div>
         </div>
-        <img src="https://m.media-amazon.com/images/I/81RCff1NpnL._AC_UY218_.jpg" alt="" />
+        <img src={image} alt="" />
         <button>Add to Basket</button>
     </div>
   )
