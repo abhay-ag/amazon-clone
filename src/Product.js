dc8bf4604dd5e55e9543d327294e337d199f34d9
abhay_ag <abhay0034.be21@chitkara.edu.in>
2022-04-04 22:46:45 +0530

    Home Componets Build | Produc Div Build

---
+++ src/Product.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import  './Product.css';
+
+function Product() {
+  return (
+    <div className="product">
+        <div className="product__info">
+            <p>The Lean Startup</p>
+            <p className="product__price">
+                <small>$</small>
+                <strong>19.99</strong>
+            </p>
+            <div className="product__rating">
+                <p>⭐</p>
+            </div>
+        </div>
+        <img src="https://m.media-amazon.com/images/I/81RCff1NpnL._AC_UY218_.jpg" alt="" />
+        <button>Add to Basket</button>
+    </div>
+  )
+}
+
+export default Product
