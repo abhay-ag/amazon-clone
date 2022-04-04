12b3f1866a25fdd2691f566c14ed7d1231651900
abhay_ag <abhay0034.be21@chitkara.edu.in>
2022-04-04 20:49:02 +0530

    Firebase.js made | Firebase set up

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDTOuWSpcawz0yOMoVkosrIfpYQHy5lBJo",
+    authDomain: "clone-99e4a.firebaseapp.com",
+    projectId: "clone-99e4a",
+    storageBucket: "clone-99e4a.appspot.com",
+    messagingSenderId: "273104756536",
+    appId: "1:273104756536:web:944ea07d53ac2022cd6bcd",
+    measurementId: "G-QGFR9QPRCS"
+  };
