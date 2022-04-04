5b38f1e2fc42276fe2ec92140c95a6d3e016377b
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-04 23:39:02 +0530

    Router Usage implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+      <Router>
+        <div className="app">
+          <Routes>
+            <Route path='/checkout' element = {[<Header />, <h1>I am a checkout page</h1>]} />
+            <Route path='/' element = {[<Header />, <Home />]} />           {/* Gets rendered out everytime rather the path is not specified */}
+          </Routes>
+        </div>
+      </Router>
   );
 }
 
