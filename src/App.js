8c03e58da5e591e0a372bd32c7eea6d1c2bfd5f9
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-05 13:35:32 +0530

    Login Component Build

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout'
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Login from './Login';
 
 function App() {
   return (
       <Router>
         <div className="app">
           <Routes>
+            <Route path='/login' element = {[<Header />, <Login/>]} />
             <Route path='/checkout' element = {[<Header />, <Checkout />]} />
             <Route path='/' element = {[<Header />, <Home />]} />           {/* Gets rendered out everytime rather the path is not specified */}
           </Routes>
