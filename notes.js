700ad8bf8de890acb9684c22464caf326b8d0bcd
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-05 20:34:19 +0530

    Woking of App.js | Header.js

---
+++ notes.js
@@ -0,0 +1,15 @@
+// App.js
+
+/*
+    1. Imports components from Checkout.js, Home.js, Header.js, Login.js
+    2. Switches between these components based on the route provided by react-router
+    3. useEffect function fires when the app components load
+    4. it is further used with firebase-tools to interact with firebase data and check the state of the user 
+*/
+
+// Header.js
+
+/*
+    1. Helps in the creation of the top navbar
+    2. Includes a searchbar, logo, and other navigatory buttons
+*/
