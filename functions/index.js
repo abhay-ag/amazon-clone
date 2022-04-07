c3573c4214575f4a41aeb60b2c4747455ef0bd02
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-07 16:24:15 +0530

    Backend Added

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,33 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors")
+const stripe = require("stripe")('sk_test_51KlrHFSAgcLd4ctWAzi00jOQ4Ra9BrNl752b0V9MzoHwId3X6BoXTsbMBvKXhmFcosgtbDrABbNMvQJRa1THFAp100tu7Y4Ewo')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// API
+
+// App config
+const app = express();
+
+// Middlewares
+app.use(cors({origin: true}));
+app.use(express.json());
+
+// API routes
+app.get('/', (request, response) => response.status(200).send('hello world'))
+app.post('/payments/create', async (request, response) => {
+    const total = request.query.total;
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: total,
+        currency: "usd",
+    });
+    // OK - CREATED
+    response.status(201).send({
+        clientSecret: paymentIntent.client_secret,
+    })
+})
+
+// Listen Commands
+exports.api = functions.https.onRequest(app)
+
+// http://localhost:5001/clone-99e4a/us-central1/api
