8e707b49a58048ef78704a99cd631a5d776cea5a
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-07 14:19:28 +0530

    Payment Page Made

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Login from './Login';
 import { useEffect } from 'react';
 import { auth } from './firebase';
 import { useStateValue } from './StateProvider';
+import Payment from './Payment';
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -37,6 +38,7 @@ function App() {
           <Routes>
             <Route path='/login' element = {[<Login/>]} />
             <Route path='/checkout' element = {[<Header />, <Checkout />]} />
+            <Route path='/payment' element = {[<Header />, <Payment />]} />
             <Route path='/' element = {[<Header />, <Home />]} />           {/* Gets rendered out everytime rather the path is not specified */}
           </Routes>
         </div>
