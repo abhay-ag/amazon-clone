26ef1ed7e8fb7a470a7481a6d09ec86285bc421a
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-07 20:33:47 +0530

    Modified the link from local to cloud

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/clone-99e4a/us-central1/api' // THE API (cloud function) URL [For local testing]
+    // baseURL: 'http://localhost:5001/clone-99e4a/us-central1/api' // THE API (cloud function) URL [For local testing]
+    baseURL: 'https://us-central1-clone-99e4a.cloudfunctions.net/api'
 });
 
 export default instance;
